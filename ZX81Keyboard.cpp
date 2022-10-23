e873f9b44d3ba3a3cd3d465494e3b9fff59acd48
Stefan <sl001@icloud.com>
2022-10-23 20:09:34 +0200

    moved files

---
+++ ZX81Keyboard.cpp
@@ -0,0 +1,178 @@
+/* 
+ *  ZX81 Keyboard for Arduino 
+ *  Stefan Lenz, 2022
+ *  
+ *  based on 
+ *    ZX81 USB Keyboard for Leonardo
+ *    (c) Dave Curran
+ *    2013-04-27
+ *
+ *    Modified with Function keys by Tony Smith
+ *    2014-02-15
+ * 
+ * for the originals see 
+ * https://blog.smittytone.net/2014/02/16/the-sinclair-zx81-a-raspberry-pi-retro-restyle-part-1/
+ * https://github.com/smittytone/ZX81Alt
+ * 
+ * Code used: keymaps
+ * 
+ * This version uses a PSKeyboard interface function from the PS2Keyboard Arduino library. Original authors are
+ *
+ * Christian Weichel <info@32leaves.net>
+ * Paul Stoffregen <paul@pjrc.com> 2010, 2011
+ * Modified for use beginning with Arduino 13 by L. Abraham Smith, <n3bah@microcompdesign.com>  
+ * 
+ *  Code used: class definition of PS2Keyboard
+ *
+ * All other code rewritten from scratch by Stefan Lenz, 2022
+ * 
+ * 
+ *
+ */
+
+#ifndef ZX81KEYBOARD_H
+#include "ZX81Keyboard.h"
+
+ZX81Keyboard::ZX81Keyboard() {}
+
+/*
+ * ZX81ROW+ZX81COL pins have to be supplied
+ * for a standard ZX81 keyboard it is 8 rows and 5 columns 
+ * i.e. 13 pins 
+ * 
+ * no state change of pinModes on construction, we do all 
+ * pinMode stuff on the fly and reset them all to INPUT after use
+ *
+ */
+void ZX81Keyboard::begin(byte *pins) {
+    for(int i=0; i<ZX81ROWS; i++) row[i]=pins[i];
+    for(int i=0; i<ZX81COLS; i++) col[i]=pins[i+ZX81ROWS];
+}
+    
+/*
+ * Returns true if there is a char to be read, false if not.
+ */
+bool ZX81Keyboard::available() {
+
+/* has there been a previous peek and a key is available */
+/* or is there a key pressed right now?*/
+    if (lastKey) return 1; else return (peek() != 0);
+}
+ 
+/*
+ * Returns the char last read from the keyboard.
+ * this only returns on key release i.e is blocking while key pressed.
+ * If there is no char available, 0 is returned.
+ */
+int ZX81Keyboard::read() {
+
+    char ch;
+
+/* do we have a key */
+    if (lastKey) ch=lastKey; else ch=peek();
+
+/* if a key is pressed wait for it to be released */
+    if (ch) {
+        while (peek()) delay(0);
+        return ch;
+    } else return 0;
+}
+
+/*
+ * peek() returns the state of the keyboard as a pressed (!) key 
+ * or 0 if there is none, peek is non blocking, SL 2022.
+ */
+int ZX81Keyboard::peek() {
+
+    uint8_t mapbuffer[40]; 
+
+/* get a debounced state of the keyboard */
+    getkey();
+
+/* a key pressed */
+    if (state_key != 255) {
+/* go to alt mode */
+        if (state_shift && state_key == ZX81SCAN_NEWLINE) {
+            alt=1; 
+            return lastKey=0;
+        }
+/* no shift and no alt */
+        if (!state_shift && !alt) {
+#ifdef USEPROGMEM
+            memcpy_P(mapbuffer, keyMap, 40);
+            return lastKey=mapbuffer[state_key];
+#else 
+            return lastKey=keyMap[state_key];
+#endif
+       } 
+/* shift and no alt */
+        if (state_shift && !alt) {
+#ifdef USEPROGMEM
+            memcpy_P(mapbuffer, keyMapShifted, 40);
+            return lastKey=mapbuffer[state_key];
+#else
+            lastKey=keyMapShifted[state_key];
+#endif
+        }
+/* alt keys */
+        if (alt) { 
+            alt=0; 
+#ifdef USEPROGMEM
+            memcpy_P(mapbuffer, keyMapAlt, 40);
+            return lastKey=mapbuffer[state_key]; 
+#else
+        return lastKey=keyMapAlt[state_key]; 
+#endif
+        }
+    } else return lastKey=0;
+}
+
+
+/* read the undebounced state of the keyboard and store it in state_shift and state_key */
+void ZX81Keyboard::scan() {
+
+/* reset the states */
+    state_key=255; 
+    state_shift=0;
+
+/* set the pins values, 5 column pin to pullup and the rest to INPUT
+ * as in the original ZX81
+ */
+    for(int i=0; i<ZX81COLS; i++) pinMode(col[i], INPUT_PULLUP);
+    for(int i=0; i<ZX81ROWS; i++) pinMode(row[i], INPUT);
+
+/* scan all shift, store the index of the first key found and the shift state*/
+    for (int i=0; i<ZX81ROWS; i++) {
+        pinMode(row[i], OUTPUT);
+        digitalWrite(row[i], LOW);
+        for (int j=0; j<ZX81COLS; j++) {
+            if (!digitalRead(col[j])) {
+                if (i == ZX81_SHIFTROW && j == ZX81_SHIFTCOL) state_shift=1; else state_key=j+ZX81COLS*i;
+            }
+        }
+        pinMode(row[i], INPUT);
+    } 
+}
+
+/* debounce the keyboard input and return if there is a stable state */
+void ZX81Keyboard::getkey() {
+
+/* scan once */
+    scan();
+
+/* remember the state */
+    long debounceTime=millis();
+    uint8_t prev_state_key=state_key;
+    uint8_t prev_state_shift=state_shift;
+
+/* see if the state remains stable */
+    while(millis()-debounceTime < ZX81_DEBOUNCE_TIME) {
+        scan();
+        if (state_key != prev_state_key || state_shift != prev_state_shift) {
+            debounceTime=millis();
+            prev_state_key=state_key;
+            prev_state_shift=state_shift;
+        }
+    } 
+}
+#endif
---
+++ ZX81Keyboard.h
@@ -0,0 +1,193 @@
+/* 
+ *  ZX81 Keyboard for Arduino 
+ *  Stefan Lenz, 2022
+ *  
+ *  based on 
+ *    ZX81 USB Keyboard for Leonardo
+ *    (c) Dave Curran
+ *    2013-04-27
+ *
+ *    Modified with Function keys by Tony Smith
+ *    2014-02-15
+ * 
+ * for the originals see 
+ * https://blog.smittytone.net/2014/02/16/the-sinclair-zx81-a-raspberry-pi-retro-restyle-part-1/
+ * https://github.com/smittytone/ZX81Alt
+ * 
+ * Code used: keymaps
+ * 
+ * This version uses a PSKeyboard interface function from the PS2Keyboard Arduino library. Original authors are
+ *
+ * Christian Weichel <info@32leaves.net>
+ * Paul Stoffregen <paul@pjrc.com> 2010, 2011
+ * Modified for use beginning with Arduino 13 by L. Abraham Smith, <n3bah@microcompdesign.com>  
+ * 
+ *  Code used: class definition of PS2Keyboard
+ *
+ * All other code rewritten from scratch by Stefan Lenz, 2022
+ * 
+ * 
+ *
+ */
+
+#define ZX81KEYBOARD_H
+#include "Arduino.h"
+
+/*
+ * 8*5 keyboard, can be extended later to other geometries 
+ */
+
+#define ZX81ROWS 8
+#define ZX81COLS 5
+
+/* where is the shift key */
+#define ZX81_SHIFTROW 5
+#define ZX81_SHIFTCOL 4
+
+/* how much time for debounce */
+#define ZX81_DEBOUNCE_TIME 10
+
+/* 
+ * Special keys mapped:
+ *  Return is 10
+ *  Backspace 8 (could also be 127)
+ *  Arrows 28-31
+ *  ZX81 specials 17 and 18 
+ */
+
+#define ZX81KEY_RETURN 10
+#define ZX81KEY_LEFT_ARROW 28
+#define ZX81KEY_ESC 27
+#define ZX81KEY_DOWN_ARROW 29
+#define ZX81KEY_UP_ARROW 30
+#define ZX81KEY_RIGHT_ARROW 31
+#define ZX81KEY_F6 18
+#define ZX81KEY_F5 17
+#define ZX81KEY_BACKSPACE 127
+
+/* the scan code of the newline key */
+#define ZX81SCAN_NEWLINE 34
+
+/* 
+ * the keymap from https://github.com/smittytone/ZX81Alt
+ */
+#define USEPROGMEM
+
+
+#ifdef USEPROGMEM
+const byte keyMap[] PROGMEM = {
+  '5', '4', '3', '2', '1', 
+  't', 'r', 'e', 'w', 'q', 
+  '6', '7', '8', '9', '0', 
+  'g', 'f', 'd', 's', 'a', 
+  'y', 'u', 'i', 'o', 'p',
+  'v', 'c', 'x', 'z', 0, /* the shift key is mapped to zero */
+  'h', 'j', 'k', 'l', ZX81KEY_RETURN,
+  'b', 'n', 'm', '.', ' '
+};
+
+/* Keymap if Shift is pressed */
+const byte keyMapShifted[] PROGMEM = {
+  ZX81KEY_LEFT_ARROW, '$', '\\', '@', ZX81KEY_ESC, 
+  'T', 'R', 'E', 'W', 'Q', 
+  ZX81KEY_DOWN_ARROW, ZX81KEY_UP_ARROW, ZX81KEY_RIGHT_ARROW, ZX81KEY_F6, ZX81KEY_BACKSPACE, 
+  'G', 'F', 'D', 'S', 'A', 
+  'Y', 'U', 'I', 'O', 'P',
+  'V', 'C', 'X', 'Z', 0, 
+  'H', 'J', 'K', 'L', ZX81KEY_F5,
+  'B', 'N', 'M', ',', ' '
+};
+
+/* keymap if Shift is pressed plus New Line
+ *Next* key read should be from this table */
+const byte keyMapAlt[] PROGMEM = {
+  ZX81KEY_LEFT_ARROW , '}' , '{' , ']' , '[', 
+  '=' , '<' , 0 , '~' , 64, 
+  ZX81KEY_DOWN_ARROW, ZX81KEY_UP_ARROW, ZX81KEY_RIGHT_ARROW, ZX81KEY_F6, ZX81KEY_BACKSPACE, 
+  0 , 0 , 0 , '|' , 156, 
+  '>', '$', '(', ')', '\"',
+  '/', '?', ';', ':', 0, 
+  '_', '-', '+', '=', ZX81KEY_RETURN,
+  '*', '<', '>', '\'', '#'
+};
+#else
+const byte keyMap[] = {
+  '5', '4', '3', '2', '1', 
+  't', 'r', 'e', 'w', 'q', 
+  '6', '7', '8', '9', '0', 
+  'g', 'f', 'd', 's', 'a', 
+  'y', 'u', 'i', 'o', 'p',
+  'v', 'c', 'x', 'z', 0, /* the shift key is mapped to zero */
+  'h', 'j', 'k', 'l', ZX81KEY_RETURN,
+  'b', 'n', 'm', '.', ' '
+};
+
+/* Keymap if Shift is pressed */
+const byte keyMapShifted[] = {
+  ZX81KEY_LEFT_ARROW, '$', '\\', '@', ZX81KEY_ESC, 
+  'T', 'R', 'E', 'W', 'Q', 
+  ZX81KEY_DOWN_ARROW, ZX81KEY_UP_ARROW, ZX81KEY_RIGHT_ARROW, ZX81KEY_F6, ZX81KEY_BACKSPACE, 
+  'G', 'F', 'D', 'S', 'A', 
+  'Y', 'U', 'I', 'O', 'P',
+  'V', 'C', 'X', 'Z', 0, 
+  'H', 'J', 'K', 'L', ZX81KEY_F5,
+  'B', 'N', 'M', ',', ' '
+};
+
+/* keymap if Shift is pressed plus New Line
+ *Next* key read should be from this table */
+const byte keyMapAlt[] = {
+  ZX81KEY_LEFT_ARROW , '}' , '{' , ']' , '[', 
+  '=' , '<' , 0 , '~' , 64, 
+  ZX81KEY_DOWN_ARROW, ZX81KEY_UP_ARROW, ZX81KEY_RIGHT_ARROW, ZX81KEY_F6, ZX81KEY_BACKSPACE, 
+  0 , 0 , 0 , '|' , 156, 
+  '>', '$', '(', ')', '\"',
+  '/', '?', ';', ':', 0, 
+  '_', '-', '+', '=', ZX81KEY_RETURN,
+  '*', '<', '>', '\'', '#'
+};
+#endif
+
+/**
+ * Class definition like in PS2Keyboard, taken from there, originally 
+ * invented by Christian Weichel
+ */
+class ZX81Keyboard {
+  public:
+    ZX81Keyboard();
+/*
+ * 8 row pins followed by 5 column pins in one array as an argument
+ */
+    void begin(byte* pins);
+    
+/*
+ * Returns true if there is a char to be read, false if not.
+*/
+    bool available();
+    
+/*
+ * Returns the char last read from the keyboard.
+ * If there is no char available, 0 is returned.
+ */
+    int read();
+
+/*
+ * peek() returns the next character 
+ * without removing it, else 0. SL 2022.
+ */
+    int peek();
+
+/* probably better private but easier to test that way*/
+    char lastKey = 0;
+    uint8_t alt = 0;
+    uint8_t state_shift = 0;
+    uint8_t state_key = 255;
+
+/* the raw keymatrix */ 
+    void getkey();
+
+  private:
+    uint8_t row[ZX81ROWS];
+    uint8_t col[ZX81COLS];
+    void scan();
+};
